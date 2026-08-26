6b886ac38a543e7d678f39b42f195a8de39df790
cekehe <51505700+cekehe@users.noreply.github.com>
2026-08-26 12:01:35 +0200

    Update to version 0.5.3

--- app/R/ImmunoAssayFinder_module_refs.R
+++ app/R/ImmunoAssayFinder_module_refs.R
@@ -143,7 +143,7 @@ refsUI <- function(id) {
     
     fluidRow(tags$p("Author: Ceke Hellström",
                     tags$br(),
-                    "Version: 0.5.2",
+                    "Version: 0.5.3",
                     tags$br(),
                     "Owner: ",
                     tags$a("Affinity Proteomics, SciLifeLab",
